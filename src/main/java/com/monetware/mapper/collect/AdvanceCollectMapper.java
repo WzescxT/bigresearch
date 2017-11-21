7697670257cbb6398d6f7e68e74aca638faed484
DerekDick <frankderekdick@gmail.com>
2017-11-21 16:11:49 +0800

    Comment @Component("userMapper") annotation out...

--- src/main/java/com/monetware/mapper/collect/AdvanceCollectMapper.java
+++ src/main/java/com/monetware/mapper/collect/AdvanceCollectMapper.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.util.ArrayList;
 
 @Mapper
-@Component(value = "userMapper")
+//@Component(value = "userMapper")
 public interface AdvanceCollectMapper {
 
     @Select("select * from spider_project_info")
